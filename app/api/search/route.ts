ef900a5787fdb02d54a3a4ddee65a8b49934c072
josueJURE <josue.jure@gmail.com>
2026-09-19 10:01:03 +0100

    (JS): use Object.fromEntries() and object destructuring as form has multiple fields

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,8 +1,10 @@
 "use server"
 
 export async function submitBookInfo(formData: FormData) {
-    const email = formData.get("title")
-    const author = formData.get("author")
-    console.log(email, author)
+    const {title, author} = Object.fromEntries(formData)
+
+  
+  
+
   
 }
