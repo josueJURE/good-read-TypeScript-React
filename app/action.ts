dab73107f15158dfc6168fb2a25a3b9ab8215b48
josueJURE <josue.jure@gmail.com>
2026-09-23 17:09:36 +0100

    (JS): set up db

--- app/action.ts
+++ app/action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import {prisma} from "../lib/prisma"
 
 const bookSchema = z.object({
   title: z.string().trim().min(1).max(50),
@@ -20,7 +21,29 @@ export async function submitBookInfo(
     return { message: "Enter a title (1–50 characters) and an author." };
   }
 
-  console.log(bookSchemaValidation.data);
+  console.log("bookSchemaValidation.data", bookSchemaValidation.data);
+
+  const {title, author} = bookSchemaValidation.data
+
+
+
+await prisma.book.createMany({
+  data: [
+    { title, author },
+   
+  ],
+
+});
+
+
+
+
 
   return { message: "Book information submitted." };
+
+
+
+
+
+
 }
